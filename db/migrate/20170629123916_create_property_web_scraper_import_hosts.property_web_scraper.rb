c783d39406db0a3272a1b126ead4b1777b302aac
Ed Tewiah <etewiah@hotmail.cim>
2017-06-29 14:40:45 +0200

    Add property_web_scraper gem

---
+++ db/migrate/20170629123916_create_property_web_scraper_import_hosts.property_web_scraper.rb
@@ -0,0 +1,17 @@
+# This migration comes from property_web_scraper (originally 20170628160331)
+class CreatePropertyWebScraperImportHosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :property_web_scraper_import_hosts do |t|
+      t.integer  :flags, default: 0, null: false
+      t.string :scraper_name
+      t.string :host
+      t.boolean :is_https
+      t.json :details, default: {}
+      t.string :slug
+      t.timestamps
+    end
+
+    add_index :property_web_scraper_import_hosts, :host, :unique => true
+
+  end
+end
